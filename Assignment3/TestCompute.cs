a2c638dabe72e372f725f218a6e1e46452ffc438
cmungol <cmungol@gmail.com>
2012-04-14 02:01:39 -0400

    remove csunit

--- Assignment3/TestCompute.cs
+++ Assignment3/TestCompute.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using csUnit;
 
 namespace Assignment3
 {
-    [TestFixture]
     public class TestCompute
     {
-        [Test]
         public void TestComputeProperties()
         {
             Compute comp = new Compute() { income = 0UL, dependent = 0UL, tax = 0.0f };
@@ -19,7 +16,6 @@ namespace Assignment3
             Assert.Equals(0UL, comp.dependent);
 
             Assert.Equals(0.0f, comp.tax);
-
         }
     }
 }
