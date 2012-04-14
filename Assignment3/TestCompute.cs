aa8cc41bf0de091c2c64a5a2075fc919dd8dad57
cmungol <cmungol@gmail.com>
2012-04-14 01:52:27 -0400

    using csUnit

---
+++ Assignment3/TestCompute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using csUnit;
+
+namespace Assignment3
+{
+    [TestFixture]
+    public class TestCompute
+    {
+        [Test]
+        public void TestComputeProperties()
+        {
+            Compute comp = new Compute() { income = 0UL, dependent = 0UL, tax = 0.0f };
+
+            Assert.Equals(0UL, comp.income);
+
+            Assert.Equals(0UL, comp.dependent);
+
+            Assert.Equals(0.0f, comp.tax);
+
+        }
+    }
+}
